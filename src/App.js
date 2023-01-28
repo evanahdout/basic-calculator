f7898ed95aa74ed10c1d0bb3bd0b003799b09a9d
Evan Ahdout <ahdoute@gmail.com>
2023-01-27 22:21:13 -0500

    Removed README

--- src/App.js
+++ src/App.js
@@ -6,18 +6,28 @@ function App() {
     const [display, setDisplay] = useState("0");
 
     function handleClear() {
-        setDisplay("")
-    }
-
-    function handleSymbol(symbol) {
-        setDisplay(symbol);
-    }
-
-    function handleAdd() {
-
+        setDisplay("0");
     }
 
     function handleEquals() {
+
+    }
+
+// problem: 
+
+    function handleNumber(symbol) {
+        if (display === '0') {
+            setDisplay(symbol);
+          } else {
+            setDisplay(display + symbol);
+          }
+        };
+
+    function handleDecimal() {
+        
+    }
+
+    function handleOperator(symbol) {
 
     }
 
@@ -27,16 +37,16 @@ function App() {
             <div id="display">{display}</div>
             <div id="symbols">
                 <button id="equals" onClick="">=</button>
-                <button id="zero" onClick={handleSymbol(0)}>0</button>
-                <button id="one">1</button>
-                <button id="two">2</button>
-                <button id="three">3</button>
-                <button id="four">4</button>
-                <button id="five">5</button>
-                <button id="six">6</button>
-                <button id="seven">7</button>
-                <button id="eight">8</button>
-                <button id="nine">9</button>
+                <button id="zero" onClick={() => handleNumber("0")}>0</button>
+                <button id="one" onClick={() => handleNumber("1")}>1</button>
+                <button id="two" onClick={() => handleNumber("2")}>2</button>
+                <button id="three" onClick={() => handleNumber("3")}>3</button>
+                <button id="four" onClick={() => handleNumber("4")}>4</button>
+                <button id="five" onClick={() => handleNumber("5")}>5</button>
+                <button id="six" onClick={() => handleNumber("6")}>6</button>
+                <button id="seven" onClick={() => handleNumber("7")}>7</button>
+                <button id="eight" onClick={() => handleNumber("8")}>8</button>
+                <button id="nine" onClick={() => handleNumber("9")}>9</button>
                 <button id="add">+</button>
                 <button id="subtract">-</button>
                 <button id="multiply">*</button>
