fb513687b1a31faf57acffa3cb76f3401ac80820
Evan Ahdout <ahdoute@gmail.com>
2023-01-31 00:35:08 -0500

    Draft complete

--- src/App.js
+++ src/App.js
@@ -7,19 +7,26 @@ function App() {
     const [result, setResult] = useState("0");
 
     const handleNumber = (symbol) => {
-        if (expression === "0") {
-            setExpression(symbol.target.value);
-        } else {
-            setExpression(expression + symbol.target.value);
-        }
-    }
+      if (expression.length === 20 || expression === "Digit Limit Met") {
+        setExpression("Digit Limit Met");
+        return;
+      }
+      if (expression === "0") {
+          setExpression(symbol.target.value);
+      } else {
+          setExpression(expression + symbol.target.value);
+      }
+    };
 
     const handleClear = () => {
         setExpression("0");
         setResult("0");
-    }
+    };
 
     const handleEquals = () => {
+        if (expression === "Digit Limit Met") {
+          return;
+        }
         const lastChar = expression[expression.length - 1];
         if (["+", "-", "*", "/"].includes(lastChar)) {
             setExpression(expression.slice(0, expression.length - 1));
@@ -36,67 +43,93 @@ function App() {
     };
 
     const handleDecimal = () => {
-        let lastOperand = "";
+        if (expression === "Digit Limit Met") {
+          return;
+        }
+        const operators = ["+", "-", "*", "/"];
+        let lastSymbol = "";
         if (expression === "0") {
-            setExpression("0.");
-            return;
+          setExpression("0.");
+          return;
         }
         for (let i = expression.length - 1; i >= 0; i--) {
-            if (["+", "-", "*", "/"].includes(expression[i])) {
-                break;
-            }
-            lastOperand = expression[i] + lastOperand;
+          if (operators.includes(expression[i])) break;
+          lastSymbol = expression[i] + lastSymbol;
         }
-        if (!lastOperand.includes(".") && !["+", "-", "*", "/"].includes(expression[expression.length - 1])) {
-            setExpression(expression + ".");
-        }
-    }
-    
-
-    const handleOperator = (symbol) => {
-        let lastOperator = expression[expression.length - 1];
-        if (expression === "0") {
-            return;
-        }
-        if (["+", "-", "*", "/"].includes(lastOperator)) {
-            if (symbol.target.value === "-" && lastOperator !== "-") {
-                setExpression(expression + symbol.target.value);
-            } else {
-                setExpression(expression.slice(0, expression.length - 1) + symbol.target.value);
-            }
-        } else {
-            setExpression(expression + symbol.target.value);
-        }
-        if (["+", "-", "*", "/"].includes(expression[expression.length - 1])) {
-            setExpression(expression.slice(0, expression.length - 1));
+        if (!lastSymbol.includes(".") && !operators.includes(expression[expression.length - 1])) {
+          setExpression(expression + ".");
+        } else if (operators.includes(expression[expression.length - 1])) {
+          setExpression(expression + "0.");
         }
     };
-    
+
+    const handleOperator = (symbol) => {
+        if (expression === "Digit Limit Met") {
+          return;
+        }
+        const lastSymbol = expression[expression.length - 1];
+        const operators = ["+", "-", "/", "*"];
+        if (expression.length > 1 && operators.includes(expression[expression.length - 2]) && operators.includes(lastSymbol)) {
+          setExpression(expression.slice(0, expression.length - 2) + symbol.target.value);
+          return;
+        }
+        if (lastSymbol === "-" && symbol.target.value === "-") {
+          setExpression(expression.slice(0, expression.length - 1) + "+");
+          return;
+        }
+        if (operators.includes(lastSymbol) && symbol.target.value === "-") {
+          setExpression(expression + symbol.target.value);
+          return;
+        }
+        if (!operators.includes(lastSymbol)) {
+          setExpression(expression + symbol.target.value);
+          return;
+        }
+        if (["+", "*", "/"].includes(lastSymbol)) {
+          setExpression(expression.slice(0, expression.length - 1) + symbol.target.value);
+          return;
+        }
+    };
+
     return (
-        <div id="Application">
-            <div id="display">{expression}</div>
-            <div id="result">{result}</div>
-            <div id="symbols">
-                <button id="zero" value="0" onClick={handleNumber}>0</button>
-                <button id="one" value="1" onClick={handleNumber}>1</button>
-                <button id="two" value="2" onClick={handleNumber}>2</button>
-                <button id="three" value="3" onClick={handleNumber}>3</button>
-                <button id="four" value="4" onClick={handleNumber}>4</button>
-                <button id="five" value="5" onClick={handleNumber}>5</button>
-                <button id="six" value="6" onClick={handleNumber}>6</button>
-                <button id="seven" value="7" onClick={handleNumber}>7</button>
-                <button id="eight" value="8" onClick={handleNumber}>8</button>
-                <button id="nine" value="9" onClick={handleNumber}>9</button>
-                <button id="add" value="+" onClick={handleOperator}>+</button>
-                <button id="subtract" value="-" onClick={handleOperator}>-</button>
-                <button id="multiply" value="*" onClick={handleOperator}>*</button>
-                <button id="divide" value="/" onClick={handleOperator}>/</button>
-                <button id="decimal" onClick={handleDecimal}>.</button>
-                <button id="equals" onClick={handleEquals}>=</button>
-                <button id="clear" onClick={handleClear}>AC</button>
+        <div id="app">
+            <div id="calculator">
+                <div id="displayAndResultWrapper">
+                    <div id="display">{expression}</div>
+                    <div id="result">{result}</div>
+                </div>
+                <div id="symbolsWrapper">
+                  <div>
+                      <button class="bigger" id="clear" onClick={handleClear}>AC</button>
+                      <button class="bigger" id="equals" onClick={handleEquals}>=</button>
+                  </div>
+                  <div>
+                      <button id="seven" value="7" onClick={handleNumber}>7</button>
+                      <button id="eight" value="8" onClick={handleNumber}>8</button>
+                      <button id="nine" value="9" onClick={handleNumber}>9</button>
+                      <button id="divide" value="/" onClick={handleOperator}>/</button>
+                  </div>
+                  <div>
+                      <button id="four" value="4" onClick={handleNumber}>4</button>
+                      <button id="five" value="5" onClick={handleNumber}>5</button>
+                      <button id="six" value="6" onClick={handleNumber}>6</button>
+                      <button id="multiply" value="*" onClick={handleOperator}>*</button>
+                  </div>
+                  <div>
+                      <button id="one" value="1" onClick={handleNumber}>1</button>
+                      <button id="two" value="2" onClick={handleNumber}>2</button>
+                      <button id="three" value="3" onClick={handleNumber}>3</button>
+                      <button id="subtract" value="-" onClick={handleOperator}>-</button>
+                  </div>
+                  <div>
+                      <button class="bigger" id="zero" value="0" onClick={handleNumber}>0</button>
+                      <button id="decimal" onClick={handleDecimal}>.</button>
+                      <button id="add" value="+" onClick={handleOperator}>+</button>
+                  </div>
+                </div>
             </div>
         </div>
-    )
+    );
 }
 
 export default App;
